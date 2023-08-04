43bba80c841a825f247e95df64ec70bffc4e0bf5
thiagoazvdo <thiagoazvdosoares@hotmail.com>
2023-08-04 14:52:13 -0300

    atualizando o README do projeto

---
+++ README.txt
@@ -0,0 +1,22 @@
+# Projeto Simples de Gerador de Senha
+
+Bem-vindo(a) esse projetinho simples que tive a ideia de realiza-lo durante uma aula de tópicos
+avançados de programação onde pretendo refatorar com operações lambda e stringBuilder.
+
+## Como executar o programa
+
+Antes de executar a aplicação, certifique-se de ter instalado em sua máquina o Java
+(Versão 17 utilizada no desenvolvimento)
+
+1. Faça o clone do repositório para a sua máquina local:
+
+2. Acesse o diretório do projeto
+
+3. Execute a classe GeradorDeSenha e insira os seguintes valores desejados no console: 
+	3.1 ~ Quantidade de números desejados na senha que será gerada. 
+	
+	3.2 ~ Quantidade de letras desejadas na senha. 
+	
+	3.3 ~ Quantidade de caracteres especiais desejados para a senha.
+	
+	
--- src/br/com/unifacisa/geradordesenhas/GeradorDeSenha.java
+++ src/br/com/unifacisa/geradordesenhas/GeradorDeSenha.java
@@ -1,13 +1,25 @@
 package br.com.unifacisa.geradordesenhas;
+import java.util.Scanner;
 
 public class GeradorDeSenha {
 
-	public static void main(String[] args) {
-		
-		Senha novaSenha = new Senha(2, 5, 1);		
-		
-		System.out.println(novaSenha.geraSenha());
+    public static void main(String[] args) {
 
-	}
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Digite a quantidade de números na senha: ");
+        int quantidadeNumeros = scanner.nextInt();
+
+        System.out.print("Digite a quantidade de letras na senha: ");
+        int quantidadeLetras = scanner.nextInt();
+
+        System.out.print("Digite a quantidade de caracteres especiais na senha: ");
+        int quantidadeCaracteresEspeciais = scanner.nextInt();
+
+        Senha novaSenha = new Senha(quantidadeNumeros, quantidadeLetras, quantidadeCaracteresEspeciais);
+
+        System.out.println("Senha gerada: " + novaSenha.geraSenha().toString());
+
+        scanner.close();
+    }
 }
